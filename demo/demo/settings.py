9169478f12a5d3a7aaddf50659de5fc20d9ad463
Sahil0215 <sahilbhardwaj0215@gmail.com>
2024-07-17 17:12:17 +0530

    commit6

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hchtq4)*n4oj1tlvjvpm^e%_rcrsla$64h8*ud527+!#i-wvrv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
